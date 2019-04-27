f0a4ffa7c5d9bec8405af2fe4179e84ddae2b277
devkudasov <dev.kudasov@gmail.com>
2019-04-27 18:24:12 +0300

    Completed child_process_demo

---
+++ courses/electron_fundamentals/child-process-demo/src/main.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const url = require('url');
+
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow = null;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        width: 400,
+        height: 100
+    });
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'ui', "index.html"),
+        protocol: "file:",
+        slashes: true
+    }));
+
+    mainWindow.on('close', () => {
+        mainWindow = null;
+    });
+});
---
+++ courses/electron_fundamentals/child-process-demo/src/ui/app.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const exec = require('child_process').exec;
+const os = require('os');
+
+function isDir(dir) {
+    try {
+        return fs.lstatSync(dir).isDirectory();
+    } catch (e) {
+        return false;
+    }
+}
+
+function checkGitStatus(dir) {
+    exec('git status', {
+        cwd: dir
+    }, (err, stdout, stderr) => {
+        if (err) setStatus('unknown');
+
+        if (/nothing to commit/.test(stdout)) setStatus('clean');
+        
+        setStatus('dirty');
+    });
+}
+
+function formatDir(dir) {
+    return /^~/.test(dir)
+        ? os.homedir() + dir.substr(1).trim()
+        : dir.trim();
+}
+
+function removeStatus() {
+    document.getElementById('status').classList.remove('unknown', 'clean', 'dirty');
+}
+
+function setStatus(status) {
+    removeStatus();
+    document.getElementById('status').classList.add(status);
+}
+
+let timer = null;
+document.getElementById('input').addEventListener('keyup', event => {
+    clearTimeout(timer);
+
+    setTimeout(_ => {
+        const dir = formatDir(event.target.value);
+
+        if (isDir(dir)) {
+            checkGitStatus(dir);
+        }
+    }, 500);
+});
