f4125fce5884a8d15c71074e180494d4c928d63f
devkudasov <dev.kudasov@gmail.com>
2019-08-02 15:25:28 +0300

    Complete 13 part

--- path/node/beginner/javascript_development_environment/webpack.config.prod.js
+++ path/node/beginner/javascript_development_environment/webpack.config.prod.js
@@ -1,27 +1,52 @@
 import path from 'path';
 import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
-  debug: true,
+  debug   : true,
   devtools: 'source-map',
-  inInfo: false,
-  entry: [
-    path.resolve(__dirname, 'src/index')
-  ],
-  terget: 'web',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-    filename: 'bundle.js'
+  inInfo  : false,
+  entry   : {
+    main  : path.resolve(__dirname, 'src/index'),
+    vendor: path.resolve(__dirname, 'src/vendor')
   },
-  plugins: [
+  target  : 'web',
+  output  : {
+    path      : path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename  : '[name].[chunkhash].js'
+  },
+  plugins : [
+    new ExtractTextPlugin('[name].[contenthash].css'),
+    new WebpackMd5Hash(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject  : true,
+      minify  : {
+        removeComments               : true,
+        removeStyleLinkTypeAttributes: true,
+        removeEmptyAttributes        : true,
+        removeRedundantAttributes    : true,
+        keepClosingSlash             : true,
+        useShortDoctype              : true,
+        collapseWhitespace           : true,
+        minifyURLs                   : true,
+        minifyCSS                    : true,
+        minifyJS                     : true
+      }
+    }),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin()
   ],
-  module: {
+  module  : {
     loaders: [
       { test: /\.js$/, exculde: /node_modules/, loaders: ['babel'] },
-      { test: /\.css$/, loaders: ['style', 'css'] }
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap') }
     ]
   }
 };
